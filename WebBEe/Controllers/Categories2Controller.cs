b9c77622a3d9a648bdd80accb94d5513014e590d
triph <triph@LAPTOP-00E2SJBB>
2023-10-24 17:39:52 +0700

    Add project files.

---
+++ WebBEe/Controllers/Categories2Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebBEe.Controllers
+{
+    public class Categories2Controller : Controller
+    {
+        // GET: Categories2
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
